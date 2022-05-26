cce74bb3897abe26b6e4625f6aba781269a77418
Andrés Muñoz <andres.munoz@idukay.com>
2022-05-26 10:06:47 -0500

    test exercise

--- tests/reverse.test.js
+++ tests/reverse.test.js
@@ -1,4 +1,4 @@
-const { reverse, average } = require('../utils/for_testing')
+const { reverse, average, dummy, totalLikes } = require('../utils/for_testing')
 
 describe('Reverse', () => {
   test('reverse of a', () => {
@@ -29,5 +29,43 @@ describe('Average', () => {
   test('average of [1, 2, 3, 4]', () => {
     const result = average([1, 2, 3, 4])
     expect(result).toBe(2.5)
+  })
+
+  test('average of []', () => {
+    const result = average([])
+    expect(result).toBe(0)
+  })
+})
+
+describe('Dummy', () => {
+  test('dummy returns one', () => {
+    const blogs = []
+
+    const result = dummy(blogs)
+    expect(result).toBe(1)
+  })
+})
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
+
+  test('when list has likes undefined', () => {
+    delete listWithOneBlog[0].likes
+    const result = totalLikes(listWithOneBlog)
+    expect(result).toBe(0)
   })
 })
--- utils/for_testing.js
+++ utils/for_testing.js
@@ -6,6 +6,8 @@ const reverse = (string) => {
 }
 
 const average = (array) => {
+  if (array.length === 0) return 0
+
   const reducer = (sum, item) => {
     return sum + item
   }
@@ -13,7 +15,22 @@ const average = (array) => {
   return array.reduce(reducer, 0) / array.length
 }
 
+const dummy = () => 1
+
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) return 0
+
+  const reducer = (sum, item) => {
+    const likes = item.likes || 0
+    return sum + likes
+  }
+
+  return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
   reverse,
   average,
+  dummy,
+  totalLikes
 }
