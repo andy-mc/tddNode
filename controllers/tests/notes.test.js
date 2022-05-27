aac18d380f8fdcbc7fa559c13031327a34151fc3
Andrés Muñoz <andres.munoz@idukay.com>
2022-05-26 20:39:58 -0500

    test

--- controllers/tests/notes.test.js
+++ controllers/tests/notes.test.js
@@ -5,7 +5,7 @@ describe('Notes', () => {
   const route = '/api/notes'
 
   describe('Get', () => {
-    fit('should return 200 and content-type application/json', async () => {
+    it('should return 200 and content-type application/json', async () => {
       const response = await test_api.get(route)
       expect(response.status).toBe(200)
       expect(response.headers['content-type']).toContain('application/json')
